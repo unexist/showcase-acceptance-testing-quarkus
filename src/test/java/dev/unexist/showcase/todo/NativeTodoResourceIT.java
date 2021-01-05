2cab8a6286bcc96c4c894375fa2c0be0dd156a46
Christoph Kappel <christoph@unexist.dev>
2021-01-05 13:10:36 +0100

    Updated tests and deps

--- src/test/java/dev/unexist/showcase/todo/NativeTodoResourceIT.java
+++ src/test/java/dev/unexist/showcase/todo/NativeTodoResourceIT.java
@@ -1,5 +1,17 @@
+/**
+ * @package Quarkus-Testing-Showcase
+ *
+ * @file Stupid native test
+ * @copyright 2019 Christoph Kappel <unexist@subforge.org>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file COPYING for details.
+ **/
+
 package dev.unexist.showcase.todo;
 
+import dev.unexist.showcase.todo.application.TodoResourceTest;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
