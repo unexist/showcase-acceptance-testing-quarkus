efbbfaef92b2e3491a79ffadf142a6f403f3c689
Christoph Kappel <christoph@unexist.dev>
2021-04-20 17:15:12 +0200

    Added new object factory handling

---
+++ src/test/java/dev/unexist/showcase/todo/domain/todo/cucumber/RunCucumberTest.java
@@ -0,0 +1,23 @@
+/**
+ * @package Quarkus-Testing-Showcase
+ *
+ * @file Todo cucumber extension
+ * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.domain.todo.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "pretty"
+)
+public class RunCucumberTest {
+}
