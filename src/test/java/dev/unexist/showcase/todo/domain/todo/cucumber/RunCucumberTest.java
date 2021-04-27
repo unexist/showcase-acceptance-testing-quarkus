8a64b984474e09dc946b32af49f11f372b930d9e
Christoph Kappel <christoph@unexist.dev>
2021-04-27 14:43:02 +0200

    Updated feature path

--- src/test/java/dev/unexist/showcase/todo/domain/todo/cucumber/RunCucumberTest.java
+++ src/test/java/dev/unexist/showcase/todo/domain/todo/cucumber/RunCucumberTest.java
@@ -17,7 +17,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "pretty"
+        plugin = "pretty",
+        features = "src/test/resources/features"
 )
 public class RunCucumberTest {
 }
