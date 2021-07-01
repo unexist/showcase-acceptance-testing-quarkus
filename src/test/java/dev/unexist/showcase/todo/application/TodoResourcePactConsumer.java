b7bbca5226b109c144761fdbec9dd0edc044f348
Christoph Kappel <christoph@unexist.dev>
2021-07-01 16:36:13 +0200

    Added pact tests

---
+++ src/test/java/dev/unexist/showcase/todo/application/TodoResourcePactConsumer.java
@@ -0,0 +1,88 @@
+/**
+ * @package Quarkus-Testing-Showcase
+ *
+ * @file Pact test consumer
+ * @copyright 2021 Christoph Kappel <unexist@subforge.org>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv3.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.application;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.unexist.showcase.todo.domain.todo.DueDate;
+import dev.unexist.showcase.todo.domain.todo.TodoBase;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "Todo-Provider")
+public class TodoResourcePactConsumer extends AbstractPactTest {
+
+    @Pact(consumer = "Todo-Consumer")
+    public RequestResponsePact createPact(PactDslWithProvider builder) {
+        return builder
+                .given("a running server")
+                .uponReceiving("POST /todo")
+                .path("/todo")
+                .method("POST")
+                .body(new PactDslJsonBody()
+                    .stringValue("title", "string")
+                    .stringValue("description", "string")
+                    .booleanType("done", true)
+                    .object("dueDate")
+                        .stringValue("due", "2021-05-07")
+                        .stringValue("start", "2021-05-07")
+                )
+                .willRespondWith()
+                .status(201)
+                .toPact();
+    }
+
+    @Test
+    void todoCheckPactTest(MockServer mockServer) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String endpoint = mockServer.getUrl() + "/todo";
+
+        HttpResponse response = Request.Post(endpoint)
+                .bodyString(mapper.writeValueAsString(createTodo()), ContentType.APPLICATION_JSON)
+                .execute().returnResponse();
+
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(201);
+    }
+
+    private TodoBase createTodo() {
+        TodoBase todo = new TodoBase();
+
+        todo.setTitle("string");
+        todo.setDescription("string");
+        todo.setDone(true);
+
+        DueDate due = new DueDate();
+
+        due.setStart(LocalDate.parse("2021-05-07"));
+        due.setDue(LocalDate.parse("2021-05-07"));
+
+        todo.setDueDate(due);
+
+        return todo;
+    }
+}
---
+++ src/test/java/dev/unexist/showcase/todo/application/TodoResourcePactProvider.java
@@ -0,0 +1,48 @@
+/**
+ * @package Quarkus-Testing-Showcase
+ *
+ * @file Pact test provider
+ * @copyright 2021 Christoph Kappel <unexist@subforge.org>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv3.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.application;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Provider("Todo-Provider")
+@PactBroker(valueResolver = AbstractPactTest.PactValueResolver.class)
+public class TodoResourcePactProvider extends AbstractPactTest {
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    @BeforeEach
+    void before(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", 8081, "/"));
+    }
+
+    @BeforeAll
+    static void setUp() {
+        startApplication();
+    }
+
+    @State("a running server")
+    public void runningState() {
+        /* All preparations done? */
+    }
+}
