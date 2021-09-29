cb4a0e41ff9dd589b60307886d800caabdf98ace
Christoph Kappel <christoph@unexist.dev>
2021-09-29 13:49:06 +0200

    Selected object factory to start Quarkus

---
+++ todo-service-cucumber-with-serenity/src/test/java/dev/unexist/showcase/todo/domain/todo/TodoCucumberFixture.java
@@ -0,0 +1,25 @@
+/**
+ * @package Showcase-Testing-Quarkus
+ *
+ * @file Todo cucumber extension
+ * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.domain.todo;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = "pretty",
+        features = "src/test/resources/features",
+        objectFactory = CdiObjectFactory.class
+)
+public class TodoCucumberFixture {
+}
