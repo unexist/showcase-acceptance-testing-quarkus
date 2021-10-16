7a8f1b636f41845df3ea01869c9fa4345070f17f
Christoph Kappel <christoph@unexist.dev>
2021-10-16 17:34:49 +0200

    Added Cluecumber reporting

--- todo-service-cucumber/src/test/java/dev/unexist/showcase/todo/domain/todo/TodoCucumberFixture.java
+++ todo-service-cucumber/src/test/java/dev/unexist/showcase/todo/domain/todo/TodoCucumberFixture.java
@@ -17,8 +17,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "pretty",
-        publish = false,
+        plugin = {"pretty", "json:target/cucumber-report/cucumber.json"},
+        publish = true,
         features = "src/test/resources/features"
 )
 public class TodoCucumberFixture {
