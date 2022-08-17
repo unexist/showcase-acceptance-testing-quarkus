d1570d33704ee37a365493ad56b90ddbc302c0b5
Christoph Kappel <christoph@unexist.dev>
2022-08-17 17:03:12 +0200

    Added example for Courgette

---
+++ todo-service-courgette/src/test/java/dev/unexist/showcase/todo/domain/todo/TodoCucumberFixture.java
@@ -0,0 +1,25 @@
+/**
+ * @package Showcase-Acceptance-Testing-Quarkus
+ *
+ * @file Todo cucumber extension
+ * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.domain.todo;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "json:target/cucumber-report/cucumber.json"},
+        publish = true,
+        features = "src/test/resources/features"
+)
+public class TodoCucumberFixture {
+}
