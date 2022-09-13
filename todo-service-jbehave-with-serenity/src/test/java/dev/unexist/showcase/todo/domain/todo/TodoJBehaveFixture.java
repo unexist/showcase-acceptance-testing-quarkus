464caa1cd7e1bbb61829012027f437c06e0d44a6
Christoph Kappel <christoph@unexist.dev>
2022-09-13 17:00:31 +0200

    Updated config

--- todo-service-jbehave-with-serenity/src/test/java/dev/unexist/showcase/todo/domain/todo/TodoJBehaveFixture.java
+++ todo-service-jbehave-with-serenity/src/test/java/dev/unexist/showcase/todo/domain/todo/TodoJBehaveFixture.java
@@ -1,7 +1,7 @@
 /**
  * @package Showcase-Acceptance-Testing-Quarkus
  *
- * @file Todo cucumber extension
+ * @file Todo JBehave extension
  * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
@@ -11,12 +11,12 @@
 
 package dev.unexist.showcase.todo.domain.todo;
 
+import net.serenitybdd.jbehave.SerenityStories;
+import net.serenitybdd.jbehave.runners.SerenityReportingRunner;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.StoryFinder;
-import org.jbehave.core.junit.JUnit4StoryRunner;
-import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
@@ -27,49 +27,15 @@ import java.util.List;
 import static org.jbehave.core.reporters.Format.CONSOLE;
 import static org.jbehave.core.reporters.Format.HTML;
 
-@RunWith(JUnit4StoryRunner.class)
-public class TodoJBehaveFixture extends JUnitStories {
+@RunWith(SerenityReportingRunner.class)
+public class TodoJBehaveFixture extends SerenityStories {
 
-    public TodoJBehaveFixture() {
-        configuredEmbedder().embedderControls()
-                .doGenerateViewAfterStories(true)
-                .doIgnoreFailureInStories(true)
-                .doIgnoreFailureInView(true)
-                .useThreads(2);
-    }
-
-    @Override
     public Configuration configuration() {
         return new MostUsefulConfiguration()
                 .useStoryReporterBuilder(new StoryReporterBuilder()
                         .withDefaultFormats()
                         .withFormats(CONSOLE, HTML));
     }
-
-    /*@Override
-    public Configuration configuration() {
-        Class<? extends Embeddable> embeddableClass = this.getClass();
-        LoadFromClasspath resourceLoader = new LoadFromClasspath(embeddableClass);
-        TableTransformers tableTransformers = new TableTransformers();
-        ParameterControls parameterControls = new ParameterControls();
-        // Start from default ParameterConverters instance
-        ParameterConverters parameterConverters = new ParameterConverters(resourceLoader, tableTransformers);
-        // factory to allow parameter conversion and loading from external resources (used by StoryParser too)
-        ExamplesTableFactory examplesTableFactory = new ExamplesTableFactory(resourceLoader,tableTransformers);
-        // add custom converters
-        parameterConverters.addConverters(new ParameterConverters.DateConverter(new SimpleDateFormat("yyyy-MM-dd")),
-                new ParameterConverters.ExamplesTableConverter(examplesTableFactory));
-        return new MostUsefulConfiguration()
-                .useStoryLoader(new LoadFromClasspath(embeddableClass))
-                .useStoryParser(new RegexStoryParser(examplesTableFactory))
-                .useStoryReporterBuilder(new StoryReporterBuilder()
-                        .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
-                        .withDefaultFormats()
-                        .withFormats(CONSOLE, TXT, HTML, XML))
-                .useParameterConverters(parameterConverters)
-                .useParameterControls(parameterControls)
-                .useTableTransformers(tableTransformers);
-    }*/
 
     @Override
     public InjectableStepsFactory stepsFactory() {
